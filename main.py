75c2d8a64527af96dfd7815d45907b9c808c50e8
IICuttingEdgeII <40517745+IICuttingEdgeII@users.noreply.github.com>
2020-05-15 22:06:04 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# Lesson 1- Data Types
+# Strings
+n = "sun"
+m = "day"
+# Integers
+a = 98
+b = 65
+# Float
+c = 5.8
+d = 6.1
+# Boolean
+# True or False
+e = True
+f = False
+
+# def addition(a, b):
+#
+#     return a + b
+#
+#
+# print(addition(n, m))
